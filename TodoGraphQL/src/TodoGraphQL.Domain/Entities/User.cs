05e8f58ab64e5d58433bb53e45133e6ae4e3aa3b
Giovanni S Araki <giovanni.saraoaraki@gmail.com>
2026-03-30 23:02:44 -0300

    fix: user role

--- TodoGraphQL/src/TodoGraphQL.Domain/Entities/User.cs
+++ TodoGraphQL/src/TodoGraphQL.Domain/Entities/User.cs
@@ -16,6 +16,8 @@ public class User
     public string Email { get; private set; } = string.Empty;
     public string PasswordHash { get; private set; } = string.Empty;
     public DateTime CreatedAt { get; private set; }
+    
+    [BsonRepresentation(BsonType.String)]
     public UserRole Role { get; private set; } = UserRole.User;
 
 
